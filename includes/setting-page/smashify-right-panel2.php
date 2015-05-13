204fbce6dfbf8eb7ad48a3435e3a9716dd52aa5b
Smashify <Smashify@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-13 01:11:21 +0000

    Initial commit.
    git-svn-id: https://plugins.svn.wordpress.org/facebook-page-likebox/tags/1.0@1159297 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/setting-page/smashify-right-panel2.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @author Smashify.co
+ * Plugin: Facebook Page Plugin
+ */
+?>
+
+<br>
+<div id="side-info-column" class="inner-sidebar">
+    <div class="postbox">
+        <h3 class="hndle"><span>Follow us</span></h3>
+
+        <div class="inside">
+            <ul>
+               <li><a href="http://smashify.co/facebook-page-plugin-for-wordpress/" target="_blank">Feedback</a></li>
+               <li><a href="http://twitter.com/SmashifyCo" target="_blank">Twitter</a></li>
+               <li><a href="https://www.facebook.com/pages/Smashify/1653200054899592" target="_blank">Facebook</a></li>
+               </li>
+            </ul>
+        </div>
+    </div>
+</div>
