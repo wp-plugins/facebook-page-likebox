ee829922d83fb4169607747fbe40e578034e08a5
Smashify <Smashify@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-13 01:11:21 +0000

    Initial commit.
    git-svn-id: https://plugins.svn.wordpress.org/facebook-page-likebox/trunk@1159297 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/setting-page/smashify-footer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author Smashify.co
+ * Plugin: Facebook Page Plugin
+ */
+?>
+
+
+</div>
+</div>
+<div class="clear">
+    <p>
+        <br/>&copy; Copyright <?php echo date("Y"); ?> <a href="http://smashify.co" target="_blank">Smashify.co</a>
+    </p>
+</div>		
