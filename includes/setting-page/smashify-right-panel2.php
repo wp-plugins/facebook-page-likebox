f769a9db27f3e6678b0282da550069abff0dc351
Smashify <Smashify@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-13 15:41:14 +0000

    Fixed Plugin home page URL
    git-svn-id: https://plugins.svn.wordpress.org/facebook-page-likebox/trunk@1159722 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/setting-page/smashify-right-panel2.php
+++ includes/setting-page/smashify-right-panel2.php
@@ -12,7 +12,7 @@
 
         <div class="inside">
             <ul>
-               <li><a href="http://smashify.co/facebook-page-plugin-for-wordpress/" target="_blank">Feedback</a></li>
+               <li><a href="http://smashify.co/facebook-page-plugin-likebox-for-wordpress/" target="_blank">Feedback</a></li>
                <li><a href="http://twitter.com/SmashifyCo" target="_blank">Twitter</a></li>
                <li><a href="https://www.facebook.com/pages/Smashify/1653200054899592" target="_blank">Facebook</a></li>
                </li>
