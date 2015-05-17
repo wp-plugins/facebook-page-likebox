aa8c26d15c697367cc2674649103ae07de68fd1c
Smashify <Smashify@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-17 19:57:58 +0000

    v1.1 commit
    git-svn-id: https://plugins.svn.wordpress.org/facebook-page-likebox/tags/1.1@1162300 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/setting-page/crunchlr-footer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author Crunchlr.com
+ * Plugin: Facebook Page Plugin
+ */
+?>
+
+
+</div>
+</div>
+<div class="clear">
+    <p>
+        <br/>&copy; Copyright <?php echo date("Y"); ?> <a href="http://crunchlr.com" target="_blank">Crunchlr.com</a>
+    </p>
+</div>		
