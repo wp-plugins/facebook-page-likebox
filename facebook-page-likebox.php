016666d4afca7de8cbb08af061eb3490b327f06d
Smashify <Smashify@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-05 14:39:57 +0000

    author change
    git-svn-id: https://plugins.svn.wordpress.org/facebook-page-likebox/trunk@1192806 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- facebook-page-likebox.php
+++ facebook-page-likebox.php
@@ -4,7 +4,7 @@ Plugin Name: Facebook Page Plugin (Likebox)
 Plugin URI: http://smashify.co/facebook-page-plugin-likebox-for-wordpress/
 Description: THE Simplest way to bring Facebook Page Plugin to WordPress with lot more Options
 Version: 1.4
-Author: SmashifyCo
+Author: Smashify
 Author URI: http://smashify.co
 */
 
