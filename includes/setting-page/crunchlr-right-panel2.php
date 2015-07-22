3f3cff6663f983c562869a89438e962d4102ae87
Smashify <Smashify@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-22 03:45:53 +0000

    minor changes
    git-svn-id: https://plugins.svn.wordpress.org/facebook-page-likebox/trunk@1203638 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/setting-page/crunchlr-right-panel2.php
+++ includes/setting-page/crunchlr-right-panel2.php
@@ -12,9 +12,9 @@
 
         <div class="inside">
             <ul>
-               <li><a href="http://smashr.org/facebook-page-plugin-likebox-for-wordpress/" target="_blank">Feedback</a></li>
-               <li><a href="http://twitter.com/SmashrOrg" target="_blank">Twitter</a></li>
-               <li><a href="https://www.facebook.com/pages/Smashrorg/399799783540503" target="_blank">Facebook</a></li>
+               <li><a href="https://sparklr.io/facebook-page-plugin-likebox-for-wordpress/" target="_blank">Feedback</a></li>
+               <li><a href="http://twitter.com/SparklrIO" target="_blank">Twitter</a></li>
+               <li><a href="https://www.facebook.com/pages/Sparklrio/933259313387014" target="_blank">Facebook</a></li>
                </li>
             </ul>
         </div>
